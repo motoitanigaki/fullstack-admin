9230094c940e60adfa254446a777301d4017a8e5
motoitanigaki <tanigakimotoi@gmail.com>
2025-09-12 14:13:54 +0900

    Regenerate migration file

--- packages/schema/drizzle/0000_createProduct_createCategory_createTag.sql
+++ packages/schema/drizzle/0000_createProduct_createCategory_createTag.sql
@@ -7,6 +7,12 @@ CREATE TABLE "categories" (
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "product_tags" (
+	"product_id" integer NOT NULL,
+	"tag_id" integer NOT NULL,
+	CONSTRAINT "product_tags_product_id_tag_id_pk" PRIMARY KEY("product_id","tag_id")
+);
+--> statement-breakpoint
 CREATE TABLE "products" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"category_id" integer NOT NULL,
@@ -20,7 +26,15 @@ CREATE TABLE "products" (
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "tags" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "product_tags" ADD CONSTRAINT "product_tags_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "product_tags" ADD CONSTRAINT "product_tags_tag_id_tags_id_fk" FOREIGN KEY ("tag_id") REFERENCES "public"."tags"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "products" ADD CONSTRAINT "products_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "idx_categories_name" ON "categories" USING btree ("name");--> statement-breakpoint
+CREATE INDEX "idx_products_name" ON "products" USING btree ("name");--> statement-breakpoint
 CREATE INDEX "idx_products_category" ON "products" USING btree ("category_id");--> statement-breakpoint
-CREATE INDEX "idx_products_status" ON "products" USING btree ("status");
+CREATE INDEX "idx_tags_name" ON "tags" USING btree ("name");
